741bf497116a75d008831a717c363ffd62b9d0c8
anpir <ap24069@edu.lu.lv>
2025-10-28 16:33:55 +0200

    (first|last)name -> $1 name

--- Andrey_Pirozhenok/assignment1.py
+++ Andrey_Pirozhenok/assignment1.py
@@ -1,63 +1,60 @@
-from typing import Callable
-
-
 def main() -> None:
     # String analysis
 
     print("Hello user!")
-    firstname = input("What is your firstname? ")
-    lastname = input("What is your lastname? ")
+    first_name = input("What is your first name? ")
+    last_name = input("What is your last name? ")
 
-    print(f"Firstname is {len(firstname)} characters long.")
-    print(f"Lastname is {len(lastname)} characters long.")
+    print(f"First name is {len(first_name)} characters long.")
+    print(f"Last name is {len(last_name)} characters long.")
 
-    if len(firstname) == 0 or len(firstname) == 0:
+    if len(first_name) == 0 or len(first_name) == 0:
         print("Refusing to process empty strings.")
         return
 
-    vowel_count = sum(1 for c in firstname if c.lower() in "aeiou")
+    vowel_count = sum(1 for c in first_name if c.lower() in "aeiou")
 
     def is_consonant(c: str) -> bool:
         return c.isascii() and c.isalpha() and c.lower() not in "aeiou"
 
-    consonant_count = sum(1 for c in firstname if is_consonant(c))
-    print(f"Firstname contains {vowel_count} vowels and {consonant_count} consonants.")
+    consonant_count = sum(1 for c in first_name if is_consonant(c))
+    print(f"First name contains {vowel_count} vowels and {consonant_count} consonants.")
 
-    print(f"First name in uppercase: {firstname.upper()}")
-    print(f"First name in lowercase: {firstname.lower()}")
+    print(f"First name in uppercase: {first_name.upper()}")
+    print(f"First name in lowercase: {first_name.lower()}")
 
-    print(f"Last name reversed: {lastname[::-1]}")
+    print(f"Last name reversed: {last_name[::-1]}")
 
     # Loop practise
 
-    print("Characters of the firstname:", end="")
-    for char in firstname:
+    print("Characters of the first name:", end="")
+    for char in first_name:
         print(" " + char, end="")
     print()
 
-    print("Removing characters from firstname:")
-    name = firstname
+    print("Removing characters from first name:")
+    name = first_name
     while len(name) > 0:
         print(f"char={name[0]} full={name}")
         name = name[1:]
 
     # Conditional statements
 
-    if len(firstname) > len(lastname):
-        print("Firstname is longer then lastname.")
-    elif len(firstname) < len(lastname):
-        print("Lastname is longer then firstname.")
+    if len(first_name) > len(last_name):
+        print("First name is longer then last name.")
+    elif len(first_name) < len(last_name):
+        print("Last name is longer then first name.")
     else:
-        print("Firstname is the same length as lastname")
+        print("First name is the same length as last name")
 
     # Personal password generator
 
-    password = f"{firstname[0]}{lastname[-1]}{len(firstname) +len(lastname)}"
+    password = f"{first_name[0]}{last_name[-1]}{len(first_name) +len(last_name)}"
     print(f"Your super secret password is {password}")
 
     # List method practise
 
-    chars = list(lastname)
+    chars = list(last_name)
     print(f"List of characters from the last name: {chars}")
 
     chars.append("*")
